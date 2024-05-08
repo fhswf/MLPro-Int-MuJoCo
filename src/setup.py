7cb4e359db1811b4c76383f7c95fa19138d719e3
steveyuwono <steveyuwono@gmail.com>
2024-05-08 13:06:50 +0200

    prep 0.0.1

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
 
 
-setup(name='mypackage',
-version='1.0.0',
-description='MLPro: Integration River',
+setup(name='mlpro-int-mujoco',
+version='0.0.1',
+description='MLPro: Integration MuJoCo',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
 license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
-packages=['mypackage'],
+packages=['mlpro_int_mujoco'],
 
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "dill",
-        "numpy",
-        "matplotlib",
-        "multiprocess",
-        "mlpro"
+        "mlpro[full]>=1.4.0",
+        "mujoco==3.1.3",
+        "lxml>=4.9.2"
     ],
 },
 
