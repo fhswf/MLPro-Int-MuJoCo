2abf640d44457456ae56069b59174b168abc2a59
steveyuwono <steveyuwono@gmail.com>
2024-05-14 15:57:45 +0200

    test

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-mujoco',
-version='0.1.0',
+version='0.1.1',
 description='MLPro: Integration MuJoCo',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
