356f7740f564acb330e079cdcd227ba3dc8b05b7
Steve Yuwono <88610818+steveyuwono@users.noreply.github.com>
2025-08-03 20:06:54 +0200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-mujoco',
-version='1.0.2',
+version='1.0.3',
 description='MLPro: Integration MuJoCo',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
@@ -12,7 +12,7 @@ packages=['mlpro_int_mujoco'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "mlpro[full]>=1.4.4",
+        "mlpro[full]>=2.1.0",
         "mujoco>=3.1.2",
         "lxml>=4.9.2"
     ],
